4efb204c7cbd0310948fe5f59c052b51d4d89220
Семён Новиков <simon.novikov@gmail.com>
2022-09-29 18:47:37 +0300

    urls fix email fix signup fix admin changes

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Group, Post
 
-
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -16,6 +16,4 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
-
-admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,6 +1,5 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.views import PasswordResetView
 from django.core.mail import send_mail
 
 from .forms import CreationForm
@@ -11,11 +10,13 @@ class SignUp(CreateView):
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
 
+def send_msg(email):
 
-class PasswordResetView(PasswordResetView):
     send_mail('Тема письма',
               'Текст письма.',
-              'from@example.com',
+              email,
               ['to@example.com'],
               fail_silently=False,
               )
+
+
