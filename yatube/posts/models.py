62acf832fff7a8dfd4a120892fa248812d428c5f
Семён Новиков <simon.novikov@gmail.com>
2022-09-29 16:06:27 +0300

    isort

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,4 +1,5 @@
 from email.headerregistry import UniqueUnstructuredHeader
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, render, redirect
-from django.urls import reverse
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import PostForm
 from .models import Group, Post, User
