4de6d03b2fbad6d93c47c6a176c3333ecfc72dc3
Семён Новиков <simon.novikov@gmail.com>
2022-09-29 16:34:17 +0300

    mail add

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import PasswordResetView
+from django.core.mail import send_mail
 
 from .forms import CreationForm
 
@@ -8,3 +10,12 @@ class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
+
+class PasswordResetView(PasswordResetView):
+    send_mail(
+    'Тема письма',
+    'Текст письма.',
+    'from@example.com',
+    ['to@example.com'],
+    fail_silently=False,
+    )
