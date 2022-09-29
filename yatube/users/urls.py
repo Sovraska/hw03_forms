fefd6cd5ee8b2843a885a700000ef7e7eae182c7
Семён Новиков <simon.novikov@gmail.com>
2022-09-29 16:23:20 +0300

    PEP 8 changes

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -13,45 +13,45 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/',
-        views.SignUp.as_view(
+         views.SignUp.as_view(
             template_name='users/signup.html'),
             name='signup'),
     path('logout/',
-        LogoutView.as_view(template_name='users/logged_out.html'),
+         LogoutView.as_view(template_name='users/logged_out.html'),
             name='logout'
     ),
     path('login/',
-        LoginView.as_view(template_name='users/login.html'),
+         LoginView.as_view(template_name='users/login.html'),
             name='login'
     ),
     path('password_change/done/',
-        PasswordChangeDoneView.as_view(
+         PasswordChangeDoneView.as_view(
             template_name='users/password_change_done.html'),
             name='password_change_done'
     ),
     path('password_change/',
-        PasswordChangeView.as_view(
+         PasswordChangeView.as_view(
             template_name='users/password_change_form.html',
             success_url=reverse_lazy('users:password_change_done')),
             name='password_change',
     ),
     path('password_reset/done/',
-        PasswordResetDoneView.as_view(
+         PasswordResetDoneView.as_view(
             template_name='users/password_reset_done.html'),
             name='password_reset_done'
     ),
     path('password_reset/',
-        PasswordResetView.as_view(
+         PasswordResetView.as_view(
             template_name='users/password_reset_form.html'),
             name='password_reset'
     ),
     path('reset/done/',
-        PasswordResetCompleteView.as_view(
+         PasswordResetCompleteView.as_view(
             template_name='users/password_reset_complete.html'),
             name='password_reset_complite'
     ),
     path('reset/<str:uidb64>/<str:token>',
-        PasswordResetConfirmView.as_view(
+         PasswordResetConfirmView.as_view(
             template_name='users/password_reset_confirm.html'),
             name='password_reset_confirm'
     )
