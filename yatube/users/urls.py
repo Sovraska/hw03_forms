bea92fb85ef19c769a3f9b8cb9eb3014665cd87a
Семён Новиков <simon.novikov@gmail.com>
2022-09-29 16:55:48 +0300

    PEP 8 chnges

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -14,7 +14,7 @@ app_name = 'users'
 urlpatterns = [
     path('signup/',
          views.SignUp.as_view(template_name='users/signup.html'),
-                              name='signup'
+         name='signup'
     ),
     path('logout/',
          LogoutView.as_view(template_name='users/logged_out.html'),
