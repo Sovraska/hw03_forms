32acdd20d8c65f7876011b95ed046854579476c6
Семён Новиков <simon.novikov@gmail.com>
2022-09-29 17:07:54 +0300

    PEP8

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -26,33 +26,33 @@ urlpatterns = [
          ),
     path('password_change/done/',
          PasswordChangeDoneView.as_view(
-         template_name='users/password_change_done.html'),
+            template_name='users/password_change_done.html'),
          name='password_change_done'
          ),
     path('password_change/',
          PasswordChangeView.as_view(
-         template_name='users/password_change_form.html',
-         success_url=reverse_lazy('users:password_change_done')),
+            template_name='users/password_change_form.html',
+            success_url=reverse_lazy('users:password_change_done')),
          name='password_change',
          ),
     path('password_reset/done/',
          PasswordResetDoneView.as_view(
-         template_name='users/password_reset_done.html'),
+            template_name='users/password_reset_done.html'),
          name='password_reset_done'
          ),
     path('password_reset/',
          PasswordResetView.as_view(
-         template_name='users/password_reset_form.html'),
+            template_name='users/password_reset_form.html'),
          name='password_reset'
          ),
     path('reset/done/',
          PasswordResetCompleteView.as_view(
-         template_name='users/password_reset_complete.html'),
+            template_name='users/password_reset_complete.html'),
          name='password_reset_complite'
          ),
     path('reset/<str:uidb64>/<str:token>',
          PasswordResetConfirmView.as_view(
-         template_name='users/password_reset_confirm.html'),
+            template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'
          )
 ]
